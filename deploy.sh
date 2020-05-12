edbb3d92d9ad4d0edb9b8cc2ba22c560bf26853d
BHARDWAJ Shantanu OBS/OCB <shantanu.bhardwaj@orange.com>
2020-05-12 23:24:28 +0530

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t prince2208/multi-client:latest -t prince2208/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t prince2208/multi-server:latest -t prince2208/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t prince2208/multi-worker:latest -t prince2208/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push prince2208/multi-client:latest
+docker push prince2208/multi-server:latest
+docker push prince2208/multi-worker:latest
+docker push  prince2208/multi-client:$SHA
+docker push  prince2208/multi-server:$SHA
+docker push  prince2208/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=prince2208/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=prince2208/multi-worker:$SHA
+kubectl set image deployments/client-deployment server=prince2208/multi-client:$SHA
